7de9f007dc09792b9b371fc9796189dea026f1f4
Mansi <myselfmansi18@gmail.com>
2020-10-01 00:01:09 +0530

    second

---
+++ Java_2.java
@@ -0,0 +1,12 @@
+import java.util.scanner;
+class Java_2{
+public static void main(String[] args){
+scanner sc=new scanner(System.in);
+int a=10;
+for(int i=0;i<a;i++)
+System.out.print(i);
+System.out.print(" ");
+}
+
+
+}
