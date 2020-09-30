76abaf509c8ca656e66cb7bc8489e1731c68bf7f
RoyNancy <31472176+RoyNancy@users.noreply.github.com>
2020-10-01 00:13:38 +0530

    variable should be own name

--- Java_2.java
+++ Java_2.java
@@ -2,8 +2,8 @@ import java.util.scanner;
 class Java_2{
 public static void main(String[] args){
 scanner sc=new scanner(System.in);
-int a=10;
-for(int i=0;i<a;i++)
+int num=12;
+for(int i=0;i<num;i++)
 System.out.print(i);
 System.out.print(" ");
 }
